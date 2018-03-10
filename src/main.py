02d0ce4e3dd59c9b3880a11c2cef05310b7512c6
Utsav Chatterjee <c.utsav1@gmail.com>
2018-03-11 01:14:50 +0530

    Same old First Commit

---
+++ src/main.py
@@ -0,0 +1,231 @@
+## import all necessary packages and functions.
+import csv  # read and write csv files
+from datetime import datetime  # operations to parse dates
+from pprint import pprint  # use to print data structures like dictionaries in
+
+
+# a nicer way than the base print function.
+
+def print_first_point(filename):
+    """
+    This function prints and returns the first data point (second row) from
+    a csv file that includes a header row.
+    """
+    # print city name for reference
+    city = filename.split('-')[0].split('/')[-1]
+    print('\nCity: {}'.format(city))
+
+    with open(filename, 'r') as f_in:
+        ## TODO: Use the csv library to set up a DictReader object. ##
+        ## see https://docs.python.org/3/library/csv.html           ##
+        trip_reader = csv.DictReader(f_in)
+
+        ## TODO: Use a function on the DictReader object to read the     ##
+        ## first trip from the data file and store it in a variable.     ##
+        ## see https://docs.python.org/3/library/csv.html#reader-objects ##
+        first_trip = next(trip_reader)
+
+        ## TODO: Use the pprint library to print the first trip. ##
+        ## see https://docs.python.org/3/library/pprint.html     ##
+        pprint(first_trip)
+    # output city name and first trip for later testing
+    return (city, first_trip)
+
+
+# list of files for each city
+data_files = ['./data/NYC-CitiBike-2016.csv',
+              './data/Chicago-Divvy-2016.csv',
+              './data/Washington-CapitalBikeshare-2016.csv', ]
+
+# print the first trip from each file, store in dictionary
+example_trips = {}
+for data_file in data_files:
+    city, first_trip = print_first_point(data_file)
+    example_trips[city] = first_trip
+
+
+def duration_in_mins(datum, city):
+    """
+    Takes as input a dictionary containing info about a single trip (datum) and
+    its origin city (city) and returns the trip duration in units of minutes.
+
+    Remember that Washington is in terms of milliseconds while Chicago and NYC
+    are in terms of seconds.
+
+    HINT: The csv module reads in all of the data as strings, including numeric
+    values. You will need a function to convert the strings into an appropriate
+    numeric type when making your transformations.
+    see https://docs.python.org/3/library/functions.html
+    """
+    #
+    # if city == "Washington":
+    #     input_data = datum['Duration (ms)']
+    #     int_datum = int(input_data)
+    #     duration = (int_datum / 1000) / 60
+    # else:
+    #     input_data = datum['tripduration']
+    #     int_datum = int(input_data)
+    #     duration = int_datum / 60
+
+    if city == "Washington":
+        return float(datum['Duration (ms)']) / 60000
+    else:
+        return float(datum['tripduration']) / 60
+
+    # return duration
+
+
+# Some tests to check that your code works. There should be no output if all of
+# the assertions pass. The `example_trips` dictionary was obtained from when
+# you printed the first trip from each of the original data files.
+tests = {'NYC': 13.9833,
+         'Chicago': 15.4333,
+         'Washington': 7.1231}
+
+for city in tests:
+    a = duration_in_mins(example_trips[city], city)
+    print(a)
+    assert abs(duration_in_mins(example_trips[city], city) - tests[city]) < .001
+
+
+def time_of_trip(datum, city):
+    """
+    Takes as input a dictionary containing info about a single trip (datum) and
+    its origin city (city) and returns the month, hour, and day of the week in
+    which the trip was made.
+
+    Remember that NYC includes seconds, while Washington and Chicago do not.
+
+    HINT: You should use the datetime module to parse the original date
+    strings into a format that is useful for extracting the desired information.
+    see https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+    """
+    days_dict = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
+    if city == "NYC":
+        trip = datetime.strptime(datum['starttime'], "%m/%d/%Y %H:%M:%S")
+        date_only = datetime.date(trip)
+        time_only = datetime.time(datetime.replace(trip, second=0))
+        month = date_only.month
+        hour = time_only.hour
+        day_of_week = days_dict[datetime.weekday(datetime.date(trip))]
+    elif city == "Chicago":
+        trip = datetime.strptime(datum['starttime'], "%m/%d/%Y %H:%M")
+        date_only = datetime.date(trip)
+        time_only = datetime.time(trip)
+        month = date_only.month
+        hour = time_only.hour
+        day_of_week = days_dict[datetime.weekday(datetime.date(trip))]
+    else:
+        trip = datetime.strptime(datum['Start date'], "%m/%d/%Y %H:%M")
+        date_only = datetime.date(trip)
+        time_only = datetime.time(trip)
+        month = date_only.month
+        hour = time_only.hour
+        day_of_week = days_dict[datetime.weekday(datetime.date(trip))]
+
+    return (month, hour, day_of_week)
+
+
+# Some tests to check that your code works. There should be no output if all of
+# the assertions pass. The `example_trips` dictionary was obtained from when
+# you printed the first trip from each of the original data files.
+tests = {'NYC': (1, 0, 'Friday'),
+         'Chicago': (3, 23, 'Thursday'),
+         'Washington': (3, 22, 'Thursday')}
+
+for city in tests:
+    assert time_of_trip(example_trips[city], city) == tests[city]
+
+
+def type_of_user(datum, city):
+    """
+    Takes as input a dictionary containing info about a single trip (datum) and
+    its origin city (city) and returns the type of system user that made the
+    trip.
+
+    Remember that Washington has different category names compared to Chicago
+    and NYC.
+    """
+
+    if city == 'Washington':
+        if datum['Member Type'] == 'Registered':
+            user_type = 'Subscriber'
+        elif datum['Member Type'] == 'Casual':
+            user_type = 'Customer'
+    else:
+        user_type = datum['usertype']
+
+    return user_type
+
+
+# Some tests to check that your code works. There should be no output if all of
+# the assertions pass. The `example_trips` dictionary was obtained from when
+# you printed the first trip from each of the original data files.
+tests = {'NYC': 'Customer',
+         'Chicago': 'Subscriber',
+         'Washington': 'Subscriber'}
+
+for city in tests:
+    assert type_of_user(example_trips[city], city) == tests[city]
+
+
+def condense_data(in_file, out_file, city):
+    """
+    This function takes full data from the specified input file
+    and writes the condensed data to a specified output file. The city
+    argument determines how the input file will be parsed.
+
+    HINT: See the cell below to see how the arguments are structured!
+    """
+
+    with open(out_file, 'w') as f_out, open(in_file, 'r') as f_in:
+        # set up csv DictWriter object - writer requires column names for the
+        # first row as the "fieldnames" argument
+        out_colnames = ['duration', 'month', 'hour', 'day_of_week', 'user_type']
+        trip_writer = csv.DictWriter(f_out, fieldnames=out_colnames)
+        trip_writer.writeheader()
+
+        ## TODO: set up csv DictReader object ##
+        trip_reader = csv.DictReader(f_in)
+
+        # collect data from and process each row
+        for row in trip_reader:
+            # set up a dictionary to hold the values for the cleaned and trimmed
+            # data point
+            new_point = {}
+
+            ## TODO: use the helper functions to get the cleaned data from  ##
+            ## the original data dictionaries.                              ##
+            ## Note that the keys for the new_point dictionary should match ##
+            ## the column names set in the DictWriter object above.         ##
+
+            new_point['duration'] = duration_in_mins(row, city)
+            new_point['month'] = time_of_trip(row, city)[0]
+            new_point['hour'] = time_of_trip(row, city)[1]
+            new_point['day_of_week'] = time_of_trip(row, city)[2]
+            new_point['user_type'] = type_of_user(row, city)
+
+            ## TODO: write the processed information to the output file.     ##
+            ## see https://docs.python.org/3/library/csv.html#writer-objects ##
+            with open(out_file, 'w', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=out_colnames)
+
+                writer.writeheader()
+                trip_writer.writerow({'duration': new_point['duration'],
+                                      'month': new_point['month'],
+                                      'hour': new_point['hour'],
+                                      'day_of_week': new_point['day_of_week'],
+                                      'user_type': new_point['user_type']})
+
+
+# Run this cell to check your work
+city_info = {'Washington': {'in_file': './data/Washington-CapitalBikeshare-2016.csv',
+                            'out_file': './data/Washington-2016-Summary.csv'},
+             'Chicago': {'in_file': './data/Chicago-Divvy-2016.csv',
+                         'out_file': './data/Chicago-2016-Summary.csv'},
+             'NYC': {'in_file': './data/NYC-CitiBike-2016.csv',
+                     'out_file': './data/NYC-2016-Summary.csv'}}
+
+for city, filenames in city_info.items():
+    condense_data(filenames['in_file'], filenames['out_file'], city)
+    print_first_point(filenames['out_file'])
